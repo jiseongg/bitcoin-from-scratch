d7dbdc79944d20af9089c99f23a13857d6c19abe
jiseongg <jiseongg301@gmail.com>
2021-01-06 19:34:40 +0900

    <feat>: define classes for secp256k1

--- ecc.py
+++ ecc.py
@@ -124,5 +124,33 @@ class Point:
             current += current
             coef >>= 1
         return result
-       
 
+
+# bitcoin-specific elliptic curve - secp256k1
+class S256Field(FieldElement):
+
+    def __init__(self, num, prime=None):
+        P = 2**256 - 2**32 - 977
+        super().__init__(num=num, prime=P)
+
+    def __repr__(self):
+        return '{:x}'.format(self.num).zfill(64)
+
+
+class S256Point(Point):
+
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(0), S256Field(7)
+        if type(x) == int:
+            super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
+        else:
+            super().__init__(x=x, y=y, a=a, b=b)
+
+    def __rmul__(self, coefficient):
+        coef = coefficient % N
+        return super().__rmul__(coef)
+
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+G = S256Point(
+        0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+        0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
