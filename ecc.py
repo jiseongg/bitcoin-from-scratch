ab732c1e8af9f7f4a6a907e390506bc561f416c2
jiseongg <jiseongg301@gmail.com>
2021-01-06 23:42:09 +0900

    <feat>: implement ECDSA
    Important modification of class impl:
    - use `self.__class__` to return inside class operations

--- ecc.py
+++ ecc.py
@@ -1,3 +1,8 @@
+from random import randint
+
+import hashlib
+import hmac
+
 
 class FieldElement:
 
@@ -24,24 +29,24 @@ class FieldElement:
         if self.prime != other.prime:
             raise TypeError('Cannot add two numbers in different Fields')
         num = (self.num + other.num) % self.prime
-        return FieldElement(num, self.prime)
+        return self.__class__(num, self.prime)
 
     def __sub__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot subtract two numbers in different Fields')
         num = (self.num - other.num) % self.prime
-        return FieldElement(num, self.prime)
+        return self.__class__(num, self.prime)
 
     def __mul__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot multiply two numbers in different Fields')
         num = (self.num * other.num) % self.prime
-        return FieldElement(num, self.prime)
+        return self.__class__(num, self.prime)
 
     def __pow__(self, exponent):
         _exponent = exponent % (self.prime - 1)
         num = pow(self.num, _exponent, self.prime)
-        return FieldElement(num, self.prime)
+        return self.__class__(num, self.prime)
 
     def __truediv__(self, other):
         """ Implementation of Fermat's little theorem
@@ -51,12 +56,12 @@ class FieldElement:
         if self.prime != other.prime:
             raise TypeError('Cannot divide two numbers in different Fields')
         num = self.num * pow(other.num, self.prime - 2, self.prime) % self.prime
-        return FieldElement(num, self.prime)
+        return self.__class__(num, self.prime)
 
     # scalar multiplication
     def __rmul__(self, coefficient):
         num = (self.num * coefficient) % self.prime
-        return FieldElement(num, self.prime)
+        return self.__class__(num, self.prime)
 
 
 class Point:
@@ -104,20 +109,20 @@ class Point:
             s = (other.y - self.y) / (other.x - self.x)
             x = s**2 - self.x - other.x
             y = s * (self.x - x) - self.y
-            return Point(x, y, self.a, self.b)
+            return self.__class__(x, y, self.a, self.b)
         elif self.y != other.y or self.y == 0 * self.x:
             # `0 * self.x`: for the case self.x is FieldElement object
-            return Point(None, None, self.a, self.b)
+            return self.__class__(None, None, self.a, self.b)
         else:
             s = (3 * self.x**2 + self.a) / (2 * self.y)
             x = s**2 - 2 * self.x
             y = s * (self.x - x) - self.y
-            return Point(x, y, self.a, self.b)
+            return self.__class__(x, y, self.a, self.b)
 
     def __rmul__(self, coefficient):
         coef = coefficient
         current = self
-        result = Point(None, None, self.a, self.b)
+        result = self.__class__(None, None, self.a, self.b)
         while coef:
             if coef & 1:
                 result += current
@@ -146,11 +151,73 @@ class S256Point(Point):
         else:
             super().__init__(x=x, y=y, a=a, b=b)
 
+    def __repr__(self):
+        if self.x is None:
+            return 'S256Point(infinity)'
+        else:
+            return 'S256Point({}, {})'.format(self.x, self.y)
+
     def __rmul__(self, coefficient):
         coef = coefficient % N
         return super().__rmul__(coef)
+
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N-2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        R = u * G + v * self
+        return R.x.num == sig.r
 
 N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
 G = S256Point(
         0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
         0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
+
+
+class Signature:
+
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+
+    def __repr__(self):
+        return 'Signature({:x},{:x})'.format(self.r, self.s)
+
+
+class PrivateKey:
+
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret*G
+
+    def hex(self):
+        return '{:x}'.format(self.secret).zfill(64)
+
+    def sign(self, z):
+        k = self.deterministic_k(z)
+        r = (k*G).x.num
+        k_inv = pow(k, N-2, N)
+        s = (z + r*self.secret) * k_inv % N
+        if s > N/2:
+            s = N - s
+        return Signature(r, s)
+
+    def deterministic_k(self, z):
+        k = b'\x00' * 32
+        v = b'\x01' * 32
+        if z > N:
+            z -= N
+        z_bytes = z.to_bytes(32, 'big')
+        secret_bytes = self.secret.to_bytes(32, 'big')
+        s256 = hashlib.sha256
+        k = hmac.new(k, v + b'\x00' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        k = hmac.new(k, v + b'\x01' + secret_bytes + z_bytes, s256).digest()
+        v = hmac.new(k, v, s256).digest()
+        while True:
+            v = hmac.new(k, v, s256).digest()
+            candidate = int.from_bytes(v, 'big')
+            if candidate >= 1 and candidate < N:
+                return candidate
+            k = hmac.new(k, v + b'\x00', s256).digest()
+            v = hmac.new(k, v, s256).digest()
