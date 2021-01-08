b3c84b631b7c620425227146df321e2e352dec62
jiseongg <jiseongg301@gmail.com>
2021-01-08 19:54:24 +0900

    <feat>: implement DER for signature

--- ecc.py
+++ ecc.py
@@ -225,6 +225,21 @@ class Signature:
     def __repr__(self):
         return 'Signature({:x},{:x})'.format(self.r, self.s)
 
+    def der(self):
+        rbin = self.r.to_bytes(32, 'big')
+        # remove all null bytes at the beginning
+        rbin = rbin.lstrip(b'\x00')
+        if rbin[0] & 0x80:
+            rbin = b'\x00' + rbin
+        result = bytes([2, len(rbin)]) + rbin
+
+        sbin = self.s.to_bytes(32, 'big')
+        # remove all null bytes at the beginning
+        sbin = sbin.lstrip(b'\x00')
+        if sbin[0] & 0x80:
+            sbin = b'\x00' + sbin
+        result += bytes([2, len(sbin)]) + sbin
+        return bytes([0x30, len(result)]) + result
 
 class PrivateKey:
 
