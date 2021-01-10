da3e27c330104b12a2e5a42393577fae255873b1
jiseongg <jiseongg301@gmail.com>
2021-01-10 12:56:34 +0900

    <feat>: add functions to util.py
    - decoding base58
    - encode and decode between varint and int

--- util.py
+++ util.py
@@ -34,6 +34,18 @@ def encode_base58_checksum(b):
     return encode_base58(b + hash256(b)[:4])
 
 
+def decode_base58(s):
+    num = 0
+    for c in s:
+        num *= 58
+        num += BASE58_ALPHABET.index(c)
+    combined = num.to_bytes(25, byteorder='big')
+    checksum = combined[-4:]
+    if hash256(combined[:-4])[:4] != checksum:
+        raise ValueError('bad address: {} {}'.format(checksum, hash256(combined[:-4])[:4]))
+    return combined[1:-4]
+
+
 def little_endian_to_int(b):
     '''takes byte sequence as a little-endian number and
     returns an integer'''
@@ -44,3 +56,40 @@ def int_to_little_endian(n, length):
     '''takes an integer and returns the little-endian
     byte sequence of length'''
     return n.to_bytes(length, 'little')
+
+
+def read_varint(s):
+    '''reads a variable integer from a stream'''
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # the next two bytes are the number
+        return little_endian_to_int(s.read(2))
+    
+    elif i == 0xfe:
+        # the next four bytes are the number
+        return little_endian_to_int(s.read(4))
+
+    elif i == 0xff:
+        # the next eight bytes are the number
+        return little_endian_to_int(s.read(8))
+    
+    else:
+        return i
+
+
+def encode_varint(i):
+    '''encodes an integer as a varint'''
+    if i < 0xfd:
+        return bytes([i])
+
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+
+    else:
+        raise ValueError('integer too large: {}'.format(i))
