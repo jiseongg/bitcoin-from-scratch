36201cd1fa17c544a8ff755239bde175e41f8297
jiseongg <jiseongg301@gmail.com>
2021-01-15 15:38:55 +0900

    <feat>: define script parsing and udpate opcodes

--- script.py
+++ script.py
@@ -9,4 +9,54 @@ class Script:
 
     @classmethod
     def parse(cls, s):
-        raise NotImplementedError
+        length = read_varint(s)
+        cmds = []
+        count = 0
+        while count < length:
+            current = s.read(1)
+            count += 1
+            current_byte = current[0]
+            if current_byte >= 1 and current_byte <= 75:
+                n = current_byte
+                cmds.append(s.read(n))
+                count += n
+            elif current_byte = 76:
+                data_length = little_endian_to_int(s.read(1))
+                cmds.append(s.read(data_length))
+                count += data_length + 1
+            elif current_byte == 77:
+                data_length = little_endian_to_int(s.read(2))
+                cmds.append(s.read(data_length))
+                count += data_length + 2
+            else:
+                op_code = current_byte
+                cmds.append(op_code)
+        if count != length:
+            raise SyntaxError('parsing script failed!')
+        return cls(cmds)
+
+    def raw_serialize(self):
+        result = b''
+        for cmd in self.cmds:
+            if type(cmd) == int:
+                result += int_to_little_endian(cmd, 1)
+            else:
+                length = len(cmd)
+                if length < 75:
+                    result += int_to_little_endian(length, 1)
+                elif length > 75 and length < 0x100:
+                    result += int_to_little_endian(76, 1)
+                    result += int_to_little_endian(length, 1)
+                elif length >= 0x100 and length <= 520:
+                    result += int_to_little_endian(77, 1)
+                    result += int_to_little_endian(length, 2)
+                else:
+                    raise ValueError('too long cmd')
+                result += cmd
+        return result
+
+    def serialize(self):
+        result = self.raw_serialize()
+        total = len(result)
+        return encode_varint(total) + result
+
